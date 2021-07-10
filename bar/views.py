fbdbd2144871eec60c7b32eee57a861a33f4f5cf
ShedrackGodson <hedrickgodson23@gmail.com>
2021-07-11 02:12:12 +0300

    custom actions estimate_sales() and estimate_profit() endpoints

--- bar/views.py
+++ bar/views.py
@@ -1,4 +1,6 @@
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from bar.serializers import (
     RegularInventoryRecordSerializer,
     TekilaInventoryRecordSerializer,
@@ -13,9 +15,49 @@ class RegularInventoryRecordViewSet(viewsets.ModelViewSet):
     serializer_class = RegularInventoryRecordSerializer
     authentication_classes = [TokenAuthentication]
 
+    @action(
+        detail=True,
+        methods=["GET"],
+    )
+    def estimate_total_cash_after_sale(self, request, pk=None):
+        return Response(
+            {"estimated_total_cash_after_sale": self.get_object().estimate_sales()},
+            status.HTTP_200_OK,
+        )
+
+    @action(
+        detail=True,
+        methods=["GET"],
+    )
+    def estimate_profit_after_sale(self, request, pk=None):
+        return Response(
+            {"estimated_profit_after_sale": self.get_object().estimate_profit()},
+            status.HTTP_200_OK,
+        )
+
 
 class TekilaInventoryRecordViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
     queryset = TekilaInventoryRecord.objects.all()
     serializer_class = TekilaInventoryRecordSerializer
     authentication_classes = [TokenAuthentication]
+
+    @action(
+        detail=True,
+        methods=["GET"],
+    )
+    def estimate_total_cash_after_sale(self, request, pk=None):
+        return Response(
+            {"estimated_total_cash_after_sale": self.get_object().estimate_sales()},
+            status.HTTP_200_OK,
+        )
+
+    @action(
+        detail=True,
+        methods=["GET"],
+    )
+    def estimate_profit_after_sale(self, request, pk=None):
+        return Response(
+            {"estimated_profit_after_sale": self.get_object().estimate_profit()},
+            status.HTTP_200_OK,
+        )
