59e74dd8b99fad786efe69a42667679e3f5c9e28
ShedrackGodson <hedrickgodson23@gmail.com>
2021-08-20 12:57:40 +0300

    removed permissions classes and authentication classes

--- core/views.py
+++ core/views.py
@@ -1,31 +1,23 @@
-from rest_framework.authentication import TokenAuthentication
 from core.models import CreditCustomer, Item, MeasurementUnit
+from rest_framework import viewsets
 from core.serializers import (
     CreditCustomerSerializer,
-    ItemSerializer,
     MeasurementUnitSerializer,
+    ItemSerializer,
 )
-from rest_framework import permissions
-from rest_framework import viewsets
 from icecream import ic
 
 
 class MeasurementUnitViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.IsAuthenticated]
     queryset = MeasurementUnit.objects.all()
     serializer_class = MeasurementUnitSerializer
-    authentication_classes = [TokenAuthentication]
 
 
 class ItemViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.IsAuthenticated]
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
-    authentication_classes = [TokenAuthentication]
 
 
 class CreditCustomerViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.IsAuthenticated]
     queryset = CreditCustomer.objects.all()
     serializer_class = CreditCustomerSerializer
-    authentication_classes = [TokenAuthentication]
