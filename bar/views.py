331b64bb0cb467267aa568b5d11201e8e4e6e381
ShedrackGodson <hedrickgodson23@gmail.com>
2021-08-25 17:15:13 +0300

    type annotations

--- bar/views.py
+++ bar/views.py
@@ -918,6 +918,7 @@ class CustomerTequilaOrderRecordPaymentViewSet(viewsets.ModelViewSet):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         data = self.perform_create(request)
+
         return Response(data, status.HTTP_201_CREATED)
 
     def perform_create(self, request):
