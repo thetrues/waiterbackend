ba546e9ed7289c3ae8a7cf9da540ce0c8bfead67
ShedrackGodson <hedrickgodson23@gmail.com>
2021-08-25 23:47:21 +0300

    structuring daily report payload

---
+++ reports/restaurant_views.py
@@ -0,0 +1,30 @@
+from restaurant.models import CustomerDishPayment
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from django.utils import timezone
+from typing import Dict
+
+
+class DailyReport(APIView):
+    """Get Daily Reports"""
+
+    permission_classes = []
+    authentication_classes = []
+
+    def get(self, request, *args, **kwargs):
+        response: Dict = {}
+        todays_date = timezone.localdate()
+
+        qs = (
+            CustomerDishPayment.objects.filter(date_paid__date=todays_date)
+            .select_related("customer_dish")
+            .prefetch_related("customer_dish__orders")
+        )
+
+        response["todays_date"] = todays_date.__str__()
+        sales: Dict = {}
+        sales["total_sales"]
+        response["sales"] = sales
+
+        return Response(response, status.HTTP_200_OK)
