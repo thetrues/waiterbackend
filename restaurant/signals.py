eeb4738cc7779e84e7e4dd4d88284fd9c8a39a3c
ShedrackGodson <hedrickgodson23@gmail.com>
2021-08-28 22:24:22 +0300

    signal first trial

--- restaurant/signals.py
+++ restaurant/signals.py
@@ -2,11 +2,12 @@ from django.db.models.signals import post_save
 from restaurant.utils import get_recipients
 from django.dispatch import receiver
 from restaurant.models import (
+    CreditCustomerDishPaymentHistory,
     MainInventoryItemRecordStockOut,
     MiscellaneousInventoryRecord,
     MainInventoryItemRecord,
 )
-from datetime import datetime
+from django.utils import timezone
 
 
 @receiver(post_save, sender=MainInventoryItemRecord)
@@ -27,7 +28,7 @@ def set_update_misc_inventory(sender, instance, created, **kwargs):
         ).exclude(pk=instance.pk).update(
             stock_status="unavailable",
             available_quantity=0,
-            date_perished=datetime.today(),
+            date_perished=timezone.localdate(),
         )
 
 
@@ -47,22 +48,24 @@ def send_notification(sender, instance, created, **kwargs):
         )
         send_notif(instance, message)
 
-    # if instance.item_record.available_quantity == 0:
-    #     item_record = instance.item_record
-    #     item_record.stock_status = "unavailable"
-    #     item_record.save()
-    #     # Send notification
-    #     message: str = "{} is out of stock. The remained quantity is {} {}".format(
-    #         instance.item_record.main_inventory_item.item.name,
-    #         instance.item_record.available_quantity,
-    #         instance.item_record.main_inventory_item.item.unit.name,
-    #     )
-
-    #     send_notif(instance, message)
-
 
 def send_notif(instance, message: str):
     instance.item_record.send_notification(
         message=message,
         recipients=get_recipients(),
     )
+
+
+@receiver(post_save, sender=CreditCustomerDishPaymentHistory)
+def update_payment_amounts(sender, instance, created, **kwargs):
+    if created:
+        obj = instance.credit_customer_dish_payment
+        obj.amount_paid = obj.amount_paid + instance.amount_paid
+        obj.save()
+
+        obj2 = instance.credit_customer_dish_payment.customer_dish_payment
+        obj2.amount_paid = obj2.amount_paid + instance.amount_paid
+        obj2.date_updated = timezone.now()
+        obj2.save()
+        # if obj2.customer_dish.get_total_price == 1:
+        #     pass
