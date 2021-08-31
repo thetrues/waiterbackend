9940235c29b4f0122dd6538ad687b6208005b76d
ShedrackGodson <hedrickgodson23@gmail.com>
2021-08-31 15:59:53 +0300

    type annotations

--- bar/views.py
+++ bar/views.py
@@ -629,7 +629,7 @@ class CustomerRegularOrderRecordPaymentViewSet(viewsets.ModelViewSet):
         methods=["GET"],
     )
     def get_all_unpaid(self, request, *args, **kwargs):
-        res: list = []
+        res: List[Dict] = []
         filtered_qs = self.queryset.filter(payment_status="unpaid")
         [
             res.append(
