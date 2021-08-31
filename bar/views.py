a3a5546d28acfcd304d5d4ecc003831c8c86661f
ShedrackGodson <hedrickgodson23@gmail.com>
2021-08-31 17:46:12 +0300

    type annotations

--- bar/views.py
+++ bar/views.py
@@ -1147,7 +1147,7 @@ class CustomerTequilaOrderRecordPaymentViewSet(viewsets.ModelViewSet):
         methods=["GET"],
     )
     def get_all_paid(self, request, *args, **kwargs):
-        res: list = []
+        res: List[Dict] = []
         filtered_qs = self.queryset.filter(payment_status="paid")
         [
             res.append(
