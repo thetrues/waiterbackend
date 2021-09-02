9f308f80d22c4e9ed0dc71425cd6b377e0f2ebe8
ShedrackGodson <hedrickgodson23@gmail.com>
2021-09-02 21:34:33 +0300

    removed uncessary comments

--- restaurant/managers.py
+++ restaurant/managers.py
@@ -1,14 +1,5 @@
-# # from restaurant.models import RestaurantPayrol
-# from restaurant import models
-# from django.db.models.aggregates import Sum
 from django.db.models import Manager
-# from django.conf import settings
 from typing import List, Dict
-
-
-# RESTAURANT_PAYROL = settings.RESTAURANT_PAYROL
-
-# RESTAURANT_PAYROL = getattr(models, RESTAURANT_PAYROL)
 
 
 class RestaurantPayrolCustomManager(Manager):
@@ -27,8 +18,3 @@ class RestaurantPayrolCustomManager(Manager):
             )
 
         return response
-
-    # def get_total(self, this_month) -> float:
-    #     return RESTAURANT_PAYROL.objects.filter(
-    #         date_paid__month=this_month.month, date_paid__year=this_month.year
-    #     ).aggregate(total=Sum("amount_paid"))["total"]
