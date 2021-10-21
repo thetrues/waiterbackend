c8c2d342ee56397f57bd55f57011d596f015f5a7
ShedrackGodson <hedrickgodson23@gmail.com>
2021-10-21 17:13:34 +0300

    Customized error response

--- bar/views.py
+++ bar/views.py
@@ -938,9 +938,9 @@ class RegularTequilaOrderRecordViewSet(viewsets.ModelViewSet):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        self.perform_create(request)
+        data = self.perform_create(request)
 
-        return Response(status.HTTP_201_CREATED)
+        return Response(data=data, status=status.HTTP_201_CREATED)
 
     def perform_create(self, request):
         orders = request.data.get("orders")
